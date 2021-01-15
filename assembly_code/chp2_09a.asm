bc3da44f8641b1a0169bc82eb2c22860144f5b97
Saqib Ali <24204968+PySaqib@users.noreply.github.com>
2021-01-15 20:26:33 +0500

    Added Chapter 02, Question 09, 10.
    Added chp2_09a.asm
    Added chp2_09b.asm
    Added chp2_09c.asm
    Added chp2_10.asm

---
+++ assembly_code/chp2_09a.asm
@@ -0,0 +1,7 @@
+[org 0x0100]
+
+mov ax, [0x0025]            ; copies the data from offset 0025 into AX
+                            ; do m2 DS:0025 in DOSBOX input to verify.
+
+mov ax, 0x4c00              ; exit.
+int 0x21
